b4306c2cc7a807ee1af4f7de898fdbb417607f3a
Alexander Serebrov <serebrov.alexandr@gmail.com>
2017-08-23 11:31:14 +0300

    Changed back to OrderedDict to preserve an order from scrapping to scrapping, changed fieldnames to more human-readable, also added writing header from item.keys() each time the item is processed (didn't find a better way to do that, but it seems to be fast operation)

--- auto_ria/auto_ria/pipelines.py
+++ auto_ria/auto_ria/pipelines.py
@@ -11,14 +11,29 @@ import csv
 class AutoRiaPipeline(object):
     def open_spider(self, spider):
         self.file = open('items.csv', 'w')
-        fieldnames = ['brand', 'model', 'author_name', 'author_phone', 'price',
-                'city', 'mileage', 'year']
-        self.writer = csv.DictWriter(self.file, delimiter=',', fieldnames=fieldnames)
-        self.writer.writeheader()
+        # fieldnames = ['brand', 'model', 'author_name', 'author_phone', 'price',
+        #        'city', 'mileage', 'year']
+        # fieldnames = {
+        #         'brand' : 'Brand',
+        #         'model': 'Model',
+        #         'author_name': 'Author Name',
+        #         'author_phone': 'Phone',
+        #         'price': 'Price',
+        #         'city': 'City',
+        #         'mileage': 'Mileage',
+        #         'year': 'Year'
+        # }
+        # fieldnames = item.fields.keys()
+        # self.writer = csv.DictWriter(self.file, delimiter=',', fieldnames=fieldnames)
+        # self.writer.writeheader()
 
     def close_spider(self, spider):
         self.file.close()
 
     def process_item(self, item, spider):
+        fieldnames = item.keys()
+        self.writer = csv.DictWriter(self.file, delimiter=',',
+                fieldnames=fieldnames)
+        self.writer.writeheader()
         self.writer.writerow(item)
         return item

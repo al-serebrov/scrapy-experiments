fbd9dc5e56e0148140e85046497c4ec331757e90
Alexander Serebrov <serebrov.alexandr@gmail.com>
2017-08-23 14:07:13 +0300

    Reworked csv pipeline using standard scrapy CsvItemExporter

--- auto_ria/auto_ria/pipelines.py
+++ auto_ria/auto_ria/pipelines.py
@@ -6,6 +6,7 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 import csv
+from scrapy.exporters import CsvItemExporter
 
 
 class AutoRiaCsvPipeline(object):
@@ -13,20 +14,14 @@ class AutoRiaCsvPipeline(object):
     headers_check = None
 
     def open_spider(self, spider):
-        self.filename = 'items.csv'
-        self.file = open(self.filename, 'w')
+        self.file = open('items.csv', 'wb')
+        self.exporter = CsvItemExporter(self.file, 'utf-8')
+        self.exporter.start_exporting()
 
     def close_spider(self, spider):
+        self.exporter.finish_exporting()
         self.file.close()
 
     def process_item(self, item, spider):
-        if self.headers_check is None:
-            with open(self.filename, 'r') as csvfile:
-                self.reader = csv.DictReader(csvfile, delimiter=',')
-                self.headers_check = self.reader.fieldnames
-                fieldnames = item.keys()
-                self.writer = csv.DictWriter(self.file, delimiter=',',
-                        fieldnames=fieldnames)
-                self.writer.writeheader()
-        self.writer.writerow(item)
-        return item
+        self.exporter.export_item(item)
+        return(item)

4c649f01c792d91f51f4b45c5070e4092d7c29f7
Alexander Serebrov <serebrov.alexandr@gmail.com>
2017-08-23 10:35:25 +0300

    Reworked pipeline to write into csv file

--- auto_ria/auto_ria/pipelines.py
+++ auto_ria/auto_ria/pipelines.py
@@ -5,17 +5,20 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-import json
+import csv
 
 
 class AutoRiaPipeline(object):
     def open_spider(self, spider):
-        self.file = open('items.jl', 'w')
+        self.file = open('items.csv', 'w')
+        fieldnames = ['brand', 'model', 'author_name', 'author_phone', 'price',
+                'city', 'mileage', 'year']
+        self.writer = csv.DictWriter(self.file, delimiter=',', fieldnames=fieldnames)
+        self.writer.writeheader()
 
     def close_spider(self, spider):
         self.file.close()
 
     def process_item(self, item, spider):
-        line = json.dumps(item, ensure_ascii=False) + '\n'
-        self.file.write(line)
+        self.writer.writerow(item)
         return item
